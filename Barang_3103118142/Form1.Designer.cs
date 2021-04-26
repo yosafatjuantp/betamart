701ef7041512dd559c3504595d17afeb1bde360c
yosafatjuantp <3103118142@student.smktelkom-pwt.sch.id>
2021-04-26 10:42:39 +0700

    betamart

---
+++ Barang_3103118142/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace Barang_3103118142
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel = new System.Windows.Forms.Panel();
+            this.txtstok = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtharga = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnamabarang = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtkode = new System.Windows.Forms.TextBox();
+            this.txtsearch = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.btnedit = new System.Windows.Forms.Button();
+            this.btnnew = new System.Windows.Forms.Button();
+            this.employeesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.appData = new Barang_3103118142.AppData();
+            this.employeeIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kodeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.namabarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hargaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stokDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.employeesTableAdapter = new Barang_3103118142.AppDataTableAdapters.EmployeesTableAdapter();
+            this.btndelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.employeesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AutoGenerateColumns = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.employeeIDDataGridViewTextBoxColumn,
+            this.kodeDataGridViewTextBoxColumn,
+            this.namabarangDataGridViewTextBoxColumn,
+            this.hargaDataGridViewTextBoxColumn,
+            this.stokDataGridViewTextBoxColumn});
+            this.dataGridView.DataSource = this.employeesBindingSource;
+            this.dataGridView.Location = new System.Drawing.Point(12, 256);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(798, 232);
+            this.dataGridView.TabIndex = 1;
+            this.dataGridView.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kode :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.txtstok);
+            this.panel.Controls.Add(this.label4);
+            this.panel.Controls.Add(this.txtharga);
+            this.panel.Controls.Add(this.label3);
+            this.panel.Controls.Add(this.txtnamabarang);
+            this.panel.Controls.Add(this.label2);
+            this.panel.Controls.Add(this.txtkode);
+            this.panel.Controls.Add(this.label1);
+            this.panel.Enabled = false;
+            this.panel.Location = new System.Drawing.Point(12, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(798, 210);
+            this.panel.TabIndex = 3;
+            // 
+            // txtstok
+            // 
+            this.txtstok.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.employeesBindingSource, "stok", true));
+            this.txtstok.Location = new System.Drawing.Point(158, 153);
+            this.txtstok.Name = "txtstok";
+            this.txtstok.Size = new System.Drawing.Size(493, 22);
+            this.txtstok.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Stok ;";
+            // 
+            // txtharga
+            // 
+            this.txtharga.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.employeesBindingSource, "harga", true));
+            this.txtharga.Location = new System.Drawing.Point(158, 110);
+            this.txtharga.Name = "txtharga";
+            this.txtharga.Size = new System.Drawing.Size(493, 22);
+            this.txtharga.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Harga :";
+            // 
+            // txtnamabarang
+            // 
+            this.txtnamabarang.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.employeesBindingSource, "namabarang", true));
+            this.txtnamabarang.Location = new System.Drawing.Point(158, 68);
+            this.txtnamabarang.Name = "txtnamabarang";
+            this.txtnamabarang.Size = new System.Drawing.Size(493, 22);
+            this.txtnamabarang.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nama Barang :";
+            // 
+            // txtkode
+            // 
+            this.txtkode.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.employeesBindingSource, "kode", true));
+            this.txtkode.Location = new System.Drawing.Point(158, 26);
+            this.txtkode.Name = "txtkode";
+            this.txtkode.Size = new System.Drawing.Size(493, 22);
+            this.txtkode.TabIndex = 0;
+            this.txtkode.TextChanged += new System.EventHandler(this.txtkode_TextChanged);
+            // 
+            // txtsearch
+            // 
+            this.txtsearch.Location = new System.Drawing.Point(122, 228);
+            this.txtsearch.Name = "txtsearch";
+            this.txtsearch.Size = new System.Drawing.Size(688, 22);
+            this.txtsearch.TabIndex = 0;
+            this.txtsearch.TextChanged += new System.EventHandler(this.txtsearch_TextChanged);
+            this.txtsearch.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtsearch_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Search";
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(719, 494);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(91, 33);
+            this.btnsave.TabIndex = 4;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btncancel
+            // 
+            this.btncancel.Location = new System.Drawing.Point(622, 494);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(91, 33);
+            this.btncancel.TabIndex = 3;
+            this.btncancel.Text = "Cancel";
+            this.btncancel.UseVisualStyleBackColor = true;
+            this.btncancel.Click += new System.EventHandler(this.btncancel_Click);
+            // 
+            // btnedit
+            // 
+            this.btnedit.Location = new System.Drawing.Point(525, 494);
+            this.btnedit.Name = "btnedit";
+            this.btnedit.Size = new System.Drawing.Size(91, 33);
+            this.btnedit.TabIndex = 2;
+            this.btnedit.Text = "Edit";
+            this.btnedit.UseVisualStyleBackColor = true;
+            this.btnedit.Click += new System.EventHandler(this.btnedit_Click);
+            // 
+            // btnnew
+            // 
+            this.btnnew.Location = new System.Drawing.Point(428, 494);
+            this.btnnew.Name = "btnnew";
+            this.btnnew.Size = new System.Drawing.Size(91, 33);
+            this.btnnew.TabIndex = 1;
+            this.btnnew.Text = "New";
+            this.btnnew.UseVisualStyleBackColor = true;
+            this.btnnew.Click += new System.EventHandler(this.btnnew_Click);
+            // 
+            // employeesBindingSource
+            // 
+            this.employeesBindingSource.DataMember = "Employees";
+            this.employeesBindingSource.DataSource = this.appData;
+            // 
+            // appData
+            // 
+            this.appData.DataSetName = "AppData";
+            this.appData.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // employeeIDDataGridViewTextBoxColumn
+            // 
+            this.employeeIDDataGridViewTextBoxColumn.DataPropertyName = "EmployeeID";
+            this.employeeIDDataGridViewTextBoxColumn.HeaderText = "EmployeeID";
+            this.employeeIDDataGridViewTextBoxColumn.Name = "employeeIDDataGridViewTextBoxColumn";
+            // 
+            // kodeDataGridViewTextBoxColumn
+            // 
+            this.kodeDataGridViewTextBoxColumn.DataPropertyName = "kode";
+            this.kodeDataGridViewTextBoxColumn.HeaderText = "kode";
+            this.kodeDataGridViewTextBoxColumn.Name = "kodeDataGridViewTextBoxColumn";
+            this.kodeDataGridViewTextBoxColumn.Width = 120;
+            // 
+            // namabarangDataGridViewTextBoxColumn
+            // 
+            this.namabarangDataGridViewTextBoxColumn.DataPropertyName = "namabarang";
+            this.namabarangDataGridViewTextBoxColumn.HeaderText = "namabarang";
+            this.namabarangDataGridViewTextBoxColumn.Name = "namabarangDataGridViewTextBoxColumn";
+            this.namabarangDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // hargaDataGridViewTextBoxColumn
+            // 
+            this.hargaDataGridViewTextBoxColumn.DataPropertyName = "harga";
+            this.hargaDataGridViewTextBoxColumn.HeaderText = "harga";
+            this.hargaDataGridViewTextBoxColumn.Name = "hargaDataGridViewTextBoxColumn";
+            // 
+            // stokDataGridViewTextBoxColumn
+            // 
+            this.stokDataGridViewTextBoxColumn.DataPropertyName = "stok";
+            this.stokDataGridViewTextBoxColumn.HeaderText = "stok";
+            this.stokDataGridViewTextBoxColumn.Name = "stokDataGridViewTextBoxColumn";
+            // 
+            // employeesTableAdapter
+            // 
+            this.employeesTableAdapter.ClearBeforeFill = true;
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(330, 494);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(92, 33);
+            this.btndelete.TabIndex = 12;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(822, 539);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnnew);
+            this.Controls.Add(this.btnedit);
+            this.Controls.Add(this.btncancel);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.txtsearch);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.dataGridView);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Barang _ 3103118142";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.employeesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.TextBox txtstok;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtharga;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtnamabarang;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtkode;
+        private System.Windows.Forms.TextBox txtsearch;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btncancel;
+        private System.Windows.Forms.Button btnedit;
+        private System.Windows.Forms.Button btnnew;
+        private AppData appData;
+        private System.Windows.Forms.BindingSource employeesBindingSource;
+        private AppDataTableAdapters.EmployeesTableAdapter employeesTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn employeeIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kodeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn namabarangDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hargaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stokDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button btndelete;
+    }
+}
+
---
+++ Barang_3103118142/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Barang_3103118142
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtsearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void txtsearch_TextChanged(object sender, EventArgs e)
+        {
+            
+            {
+                if (string.IsNullOrEmpty(txtsearch.Text))
+                    dataGridView.DataSource = employeesBindingSource;
+                else
+                {
+                    var query = from o in this.appData.Employees
+                                where o.kode.Contains(txtsearch.Text) || o.namabarang == txtsearch.Text || o.harga == txtsearch.Text || o.stok.Contains(txtsearch.Text)
+                                select o;
+                    dataGridView.DataSource = query.ToList();
+                }
+            }
+        }
+
+        private void dataGridView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Delete)
+            {
+                if (MessageBox.Show("Are you sure want to delete this record ? ", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    employeesBindingSource.RemoveCurrent();
+            }
+        }
+
+        private void btnnew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                panel.Enabled = true;
+                txtkode.Focus();
+                this.appData.Employees.AddEmployeesRow(this.appData.Employees.NewEmployeesRow());
+                employeesBindingSource.MoveLast();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                employeesBindingSource.ResetBindings(false);
+            }
+        }
+
+        private void btnedit_Click(object sender, EventArgs e)
+        {
+            panel.Enabled = true;
+            txtkode.Focus();
+        }
+
+        private void btncancel_Click(object sender, EventArgs e)
+        {
+            panel.Enabled = false;
+            employeesBindingSource.ResetBindings(false);
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                employeesBindingSource.EndEdit();
+                employeesTableAdapter.Update(this.appData.Employees);
+                panel.Enabled = false;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                employeesBindingSource.ResetBindings(false);
+            }
+
+        }
+
+        private void txtkode_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'appData.Employees' table. You can move, or remove it, as needed.
+            this.employeesTableAdapter.Fill(this.appData.Employees);
+            employeesBindingSource.DataSource = this.appData.Employees;
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Yakin mo Dihapus?","Message",MessageBoxButtons.YesNo,MessageBoxIcon.Question)==DialogResult.Yes)
+            {
+                employeesBindingSource.RemoveCurrent();
+                employeesBindingSource.EndEdit();
+                employeesTableAdapter.Update(this.appData.Employees);
+            }
+            else
+            {
+                employeesBindingSource.EndEdit();
+                employeesTableAdapter.Update(this.appData.Employees);
+                dataGridView.Refresh();
+            }
+        }
+    }
+}
